a83452c5384ebe1282fd905e208a190951f908bf
Stefano Bisio <s.b.milano@gmail.com>
2017-06-29 14:16:18 +0100

    Logic and img fixed

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ $(document).ready(function () {
             //      Prevents jquery from deleting it immediately
             event.preventDefault();
         } else {
-            event.preventDefault();
 
         }
     }
